549f4283392150ba181260293113d2ccd2a6e852
alyssah38 <alyssa.hunt@ufl.edu>
2023-10-25 17:39:59 -0400

    Alyssa's encoder (fixed 7,8,9)

--- lab6alyssa_encode.py
+++ lab6alyssa_encode.py
@@ -4,7 +4,14 @@ user_password = list(input())
 def encoder(password):
     encoded_password = []
     for i in range(0, len(password)):
-        encoded_password.append(int(password[i])+3)
+        if password[i] == '7':
+            encoded_password.append('0')
+        elif password[i] == '8':
+            encoded_password.append('1')
+        elif password[i] == '9':
+            encoded_password.append('2')
+        else:
+            encoded_password.append(int(password[i]) + 3)
         i += 1
     encoded_password_string = ''
     for i in range(0, len(encoded_password)):
